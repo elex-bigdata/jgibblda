d98cf02f06e1e28b16bd7e7750e00c0adc572a89
boyang070822 <boyang070822@gmail.com>
2014-03-26 17:16:27 +0800

    shells

---
+++ bin/estOrigin.sh
@@ -0,0 +1,8 @@
+echo $*
+echo $@
+args=$*
+baseDir=`dirname $0`/..
+JAR=$baseDir/target/jgibblda-jar-with-dependencies.jar
+MAIN=com.elex.bigdata.jgibblda.LDA
+echo "java -cp $JAR $MAIN -est -dir /data/log/user_category/llda -ntopics 20 $args"
+java -cp $JAR $MAIN -est -dir /data/log/user_category/llda -ntopics 20 $args
