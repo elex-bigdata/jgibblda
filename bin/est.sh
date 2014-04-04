cf38a4131e2c47bfdc2fc0b7ab24287bb800b1c3
boyang070822 <boyang070822@gmail.com>
2014-04-04 16:49:56 +0800

    script est.sh

--- bin/est.sh
+++ bin/est.sh
@@ -4,6 +4,6 @@ args=$*
 baseDir=`dirname $0`/..
 JAR=$baseDir/target/jgibblda-jar-with-dependencies.jar
 MAIN=com.elex.bigdata.jgibblda.LDA
-logFile=/data/log/user_category/processLog/llda/est.log
-echo "java -cp $JAR $MAIN -est -dir /data/log/user_category/llda  -model elex -ntopics 5 $args >> $logFile 2>&1"
-java -cp $JAR $MAIN -est -dir /data/log/user_category/llda  -model elex -ntopics 5 $args >> $logFile 2>&1
+logFile=/data/user_category/llda/logs/est.log
+echo "java -cp $JAR $MAIN -est -modelDir /data/user_category/llda/models -docDir /data/user_category/llda/docs -model elex -ntopics 5 $args >> $logFile 2>&1"
+java -cp $JAR $MAIN -est -modelDir /data/user_category/llda/models -docDir /data/user_category/llda/docs -model elex -ntopics 5 $args >> $logFile 2>&1
--- bin/estc.sh
+++ bin/estc.sh
@@ -4,6 +4,6 @@ args=$*
 baseDir=`dirname $0`/..
 JAR=$baseDir/target/jgibblda-jar-with-dependencies.jar
 MAIN=com.elex.bigdata.jgibblda.LDA
-logFile=/data/log/user_category/processLog/llda/estc.log
-echo "java -cp $JAR $MAIN -estc -dir /data/log/user_category/llda/ -ntopics 5 -model elex $args >> $logFile 2>&1"
-java -cp $JAR $MAIN -estc -dir /data/log/user_category/llda -ntopics 5 -model elex $args  >> $logFile 2>&1
+logFile=/data/user_category/llda/logs/estc.log
+echo "java -cp $JAR $MAIN -estc -modelDir /data/user_category/llda/models -docDir /data/user_category/llda/docs -model elex -ntopics 5 $args >> $logFile 2>&1"
+java -cp $JAR $MAIN -estc -modelDir /data/user_category/llda/models -docDir /data/user_category/llda/docs -model elex -ntopics 5 $args >> $logFile 2>&1
