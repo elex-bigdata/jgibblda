0c9a361de1522ccce82627fe1687c64121973d4f
boyang070822 <boyang070822@gmail.com>
2014-04-08 11:41:07 +0800

    change args

--- bin/etl.sh
+++ bin/etl.sh
@@ -1,5 +1,6 @@
+args=$*
 baseDir=`dirname $0`/..
 JAR=$baseDir/target/jgibblda-jar-with-dependencies.jar
 MAIN=com.elex.bigdata.jgibblda.ResultEtl
-echo "java -cp $JAR $MAIN $1"
-java -cp $JAR $MAIN $1
+echo "java -cp $JAR $MAIN -modelDir /data/user_category/llda/models "
+java -cp $JAR $MAIN -modelDir /data/user_category/llda/models $args
