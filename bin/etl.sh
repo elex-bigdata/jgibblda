c3c83d21096a509f297691d584f16c4282e8c6a4
boyang070822 <boyang070822@gmail.com>
2014-04-08 11:50:22 +0800

    etl.sh

--- bin/etl.sh
+++ bin/etl.sh
@@ -2,5 +2,5 @@ args=$*
 baseDir=`dirname $0`/..
 JAR=$baseDir/target/jgibblda-jar-with-dependencies.jar
 MAIN=com.elex.bigdata.jgibblda.ResultEtl
-echo "java -cp $JAR $MAIN -modelDir /data/user_category/llda/models "
+echo "java -cp $JAR $MAIN -modelDir /data/user_category/llda/models $args "
 java -cp $JAR $MAIN -modelDir /data/user_category/llda/models $args
