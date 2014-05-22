59ae90113ea157727a5593d393c5d041a4c0f141
boyang070822 <boyang070822@gmail.com>
2014-05-22 18:35:45 +0800

    change shells

--- bin/estc.sh
+++ bin/estc.sh
@@ -5,7 +5,7 @@ baseDir=`dirname $0`/..
 JAR=$baseDir/target/jgibblda-jar-with-dependencies.jar
 MAIN=com.elex.bigdata.jgibblda.LDA
 logFile=/data/user_category/llda/logs/estc.log
-echo "java -cp $JAR $MAIN -estc -modelDir /data/user_category/llda/models -docDir /data/user_category/llda/docs -model elex -ntopics 6 $args >> $logFile 2>&1"
-echo "`date` java -cp $JAR $MAIN -estc -modelDir /data/user_category/llda/models -docDir /data/user_category/llda/docs -model elex -ntopics 6 $args >> $logFile 2>&1" >> $logFile
-java -cp $JAR $MAIN -estc -modelDir /data/user_category/llda/models -docDir /data/user_category/llda/docs -model elex -ntopics 6 $args >> $logFile 2>&1
+echo "java -cp $JAR $MAIN -estc -modelDir /data/user_category/llda/models -docDir /data/user_category/llda/docs -model elex -ntopics 8 $args >> $logFile 2>&1"
+echo "`date` java -cp $JAR $MAIN -estc -modelDir /data/user_category/llda/models -docDir /data/user_category/llda/docs -model elex -ntopics 8 $args >> $logFile 2>&1" >> $logFile
+java -cp $JAR $MAIN -estc -modelDir /data/user_category/llda/models -docDir /data/user_category/llda/docs -model elex -ntopics 8 $args >> $logFile 2>&1
 echo "end time `date`" >> $logFile
--- bin/infSep.sh
+++ bin/infSep.sh
@@ -5,7 +5,7 @@ baseDir=`dirname $0`/..
 JAR=$baseDir/target/jgibblda-jar-with-dependencies.jar
 MAIN=com.elex.bigdata.jgibblda.LDA
 logFile=/data/user_category/llda/logs/inf.log
-echo "java -cp $JAR $MAIN -infseparately -modelDir /data/user_category/llda/models -docDir /data/user_category/llda/docs -ntopics 6 -model elex  $args >> $logFile 2>&1"
-echo "`date` java -cp $JAR $MAIN -infseparately -modelDir /data/user_category/llda/models -docDir /data/user_category/llda/docs -ntopics 6 -model elex  $args >> $logFile 2>&1" >> $logFile
-java -cp $JAR $MAIN -infseparately -modelDir /data/user_category/llda/models -docDir /data/user_category/llda/docs -ntopics 6 -model elex  $args >> $logFile 2>&1
+echo "java -cp $JAR $MAIN -infseparately -modelDir /data/user_category/llda/models -docDir /data/user_category/llda/docs -ntopics 8 -model elex  $args >> $logFile 2>&1"
+echo "`date` java -cp $JAR $MAIN -infseparately -modelDir /data/user_category/llda/models -docDir /data/user_category/llda/docs -ntopics 8 -model elex  $args >> $logFile 2>&1" >> $logFile
+java -cp $JAR $MAIN -infseparately -modelDir /data/user_category/llda/models -docDir /data/user_category/llda/docs -ntopics 8 -model elex  $args >> $logFile 2>&1
 echo "end time `date`" >> $logFile
